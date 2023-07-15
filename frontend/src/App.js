6ba26051d6b260f9a526fd946967dd21d95934e9
Arslan <arslanm147@gmail.com>
2023-07-15 11:50:56 +0500

    added landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
+import LandingPage from "./screens/landing-page/landing-page";
 
 function App() {
   return (
     <>
       <Header />
-      <main style={{ minHeight: "93vh" }}></main>
+      <LandingPage />
       <Footer />
     </>
   );
--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Form, Button} from "react-bootstrap";
+
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Navbar bg = "primary" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="m-auto">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">My Notes</Nav.Link>
+            <NavDropdown title="Arslan" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+              Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
